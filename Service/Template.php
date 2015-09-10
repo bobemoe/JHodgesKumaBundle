005a325758a98aba015e4cfaa2b0da672d8256b9
lewisphillips <$&Oun7ec1>
2015-09-10 18:49:38 +0100

    Changing Menu to fix for 3.3 ebcause of BC Break

--- Service/Template.php
+++ Service/Template.php
@@ -1,51 +1,55 @@
 <?php
 
-namespace JHodges\KumaBundle\Service;
+    namespace JHodges\KumaBundle\Service;
 
-use Doctrine\ORM\EntityManager;
-use Symfony\Component\Security\Core\SecurityContextInterface;
-use Kunstmaan\AdminBundle\Helper\Security\Acl\AclHelper;
+    use Doctrine\ORM\EntityManager;
+    use Symfony\Component\Security\Core\SecurityContextInterface;
+    use Kunstmaan\AdminBundle\Helper\Security\Acl\AclHelper;
 
-use Kunstmaan\AdminBundle\Helper\Security\Acl\Permission\PermissionMap;
-use Kunstmaan\NodeBundle\Helper\NodeMenu;
+    use Kunstmaan\AdminBundle\Helper\Security\Acl\Permission\PermissionMap;
+    use Kunstmaan\NodeBundle\Helper\NodeMenu;
 
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+    use Symfony\Component\HttpKernel\Exception\HttpException;
+    use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-class Template{
+    class Template{
 
-    public function __construct(EntityManager $em, SecurityContextInterface $securityContext, AclHelper $aclHelper){
-        $this->em=$em;
-        $this->securityContext=$securityContext;
-        $this->aclHelper=$aclHelper;
+        public function __construct(EntityManager $em, SecurityContextInterface $securityContext, AclHelper $aclHelper, NodeMenu $nodeMenu){
+            $this->em=$em;
+            $this->securityContext=$securityContext;
+            $this->aclHelper=$aclHelper;
+            $this->nodeMenu = $nodeMenu;
+        }
+
+        public function getVars($url,$locale,$vars=array()){
+            $nodeTranslation = $this->em->getRepository('KunstmaanNodeBundle:NodeTranslation')->getNodeTranslationForUrl($url, $locale);
+
+            if (!$nodeTranslation) {
+                throw new HttpException(404,'No page found for slug ' . $url);
+            }
+
+            if (!$nodeTranslation->isOnline()) {
+                throw new HttpException(404,'The requested page is not online');
+            }
+
+            $node   = $nodeTranslation->getNode();
+            if (false === $this->securityContext->isGranted(PermissionMap::PERMISSION_VIEW, $node)) {
+                throw new AccessDeniedException('You do not have sufficient rights to access this page.');
+            }
+
+            $entity = $nodeTranslation->getPublicNodeVersion()->getRef($this->em);
+
+            $nodeMenu = $this->nodeMenu;
+            $nodeMenu->setLocale($locale);
+            $nodeMenu->setCurrentNode($node);
+
+            return array_merge($vars,array(
+                'nodetranslation' => $nodeTranslation,
+                'slug'            => $url,
+                'page'            => $entity,
+                'resource'        => $entity,
+                'nodemenu'        => $nodeMenu,
+            ));
+        }
+
     }
-
-    public function getVars($url,$locale,$vars=array()){
-        $nodeTranslation = $this->em->getRepository('KunstmaanNodeBundle:NodeTranslation')->getNodeTranslationForUrl($url, $locale);
-
-        if (!$nodeTranslation) {
-            throw new HttpException(404,'No page found for slug ' . $url);
-        }
-
-        if (!$nodeTranslation->isOnline()) {
-            throw new HttpException(404,'The requested page is not online');
-        }
-
-        $node   = $nodeTranslation->getNode();
-        if (false === $this->securityContext->isGranted(PermissionMap::PERMISSION_VIEW, $node)) {
-            throw new AccessDeniedException('You do not have sufficient rights to access this page.');
-        }
-
-        $entity = $nodeTranslation->getPublicNodeVersion()->getRef($this->em);
-        $nodeMenu = new NodeMenu($this->em, $this->securityContext, $this->aclHelper, $locale, $node, PermissionMap::PERMISSION_VIEW, $includeOffline=false);
-
-        return array_merge($vars,array(
-            'nodetranslation' => $nodeTranslation,
-            'slug'            => $url,
-            'page'            => $entity,
-            'resource'        => $entity,
-            'nodemenu'        => $nodeMenu,
-        ));
-    }
-
-}
